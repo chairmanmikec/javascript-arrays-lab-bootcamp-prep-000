4ed9781c2fbc22bf74b1d83cb959e5c8fc077e02
chairmanmikec <michaelc1012@gmail.com>
2018-02-01 16:42:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const app = "I don't do much."
+
+function destructivelyAppendKitten(array,name){
+  array.push(name)
+  return array
+}
+
+function destructivelyPrependKitten(array,name){
+  array.unshift(name)
+  return array
+}
